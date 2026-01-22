fbd85618c5f0f19f805bb12f08962ecb4938f019
Scott Davis <scottdavisxx@gmail.com>
2026-01-21 19:28:09 -0700

    delete hero banner and page content, fix tsconfig

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,10 @@
-import { Suspense } from 'react'
-import Link from 'next/link'
-import { PortableText } from '@portabletext/react'
-
-import { AllPosts } from '@/app/components/Posts'
-import { settingsQuery } from '@/sanity/lib/queries'
-import { sanityFetch } from '@/sanity/lib/live'
-import { dataAttr } from '@/sanity/lib/utils'
+import HeroBanner from "./components/HeroBanner";
 
 export default async function Page() {
-  const { data: settings } = await sanityFetch({
-    query: settingsQuery,
-  })
 
   return (
-    <h1 className='text-center text-4xl font-bold mt-24'>Index Page</h1>
+    <>
+      <HeroBanner />
+    </>
   )
 }
