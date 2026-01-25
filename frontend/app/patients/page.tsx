e688a57499f3374b9516976cb5f14295d1ec96e0
Scott Davis <scottdavisxx@gmail.com>
2026-01-24 17:11:39 -0700

    Add Patients and Practitioners pages with HeroBanner integration; include placeholder images for both pages.

---
+++ frontend/app/patients/page.tsx
@@ -0,0 +1,9 @@
+import HeroBanner from "../components/HeroBanner";
+
+export default function Patients() {
+  return (
+    <>
+      <HeroBanner title="Patients" subtitle="Patients" cta={[]} image="/fpo-hero-banner.png" bgImage="/fpo-hero-banner-bg.png" altText="Hero Banner Image" />
+    </>
+  )
+}
---
+++ frontend/app/practitioners/page.tsx
@@ -0,0 +1,9 @@
+import HeroBanner from "../components/HeroBanner";
+
+export default function Practitioners() {
+  return (
+    <>
+      <HeroBanner title="Practitioners" subtitle="Practitioners" cta={[]} image="/fpo-hero-banner.png" bgImage="/fpo-hero-banner-bg.png" altText="Hero Banner Image" />
+    </>
+  )
+}
