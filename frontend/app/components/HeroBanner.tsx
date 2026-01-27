86bbfe1c89c92f8c388175eac2221fcf4f697a83
Scott Davis <scottdavisxx@gmail.com>
2026-01-26 19:31:53 -0700

    Refactor HeroBanner component to adjust layout and styling; reduce height, modify width, and improve image positioning for better responsiveness.

--- frontend/app/components/HeroBanner.tsx
+++ frontend/app/components/HeroBanner.tsx
@@ -13,8 +13,8 @@ interface HeroBannerProps {
 
 export default function HeroBanner({ title, subtitle, cta, image, bgImage, altText }: HeroBannerProps) {
   return (
-    <div className="flex items-center justify-between h-[800px] bg-brand-blue text-white pl-20 overflow-visible">
-      <div className="flex flex-col gap-8 w-1/2">
+    <div className="flex items-center justify-between h-[630px] bg-brand-blue text-white pl-20 overflow-hidden relative mt-20">
+      <div className="flex flex-col gap-8 w-[45%] mb-20">
         <h1 className="text-6xl text-white leading-snug">{title}</h1>
         <h2 className="text-2xl font-satoshi text-white leading-snug font-normal">{subtitle}</h2>
         <div className="flex gap-6">
@@ -23,10 +23,12 @@ export default function HeroBanner({ title, subtitle, cta, image, bgImage, altTe
           ))}
         </div>
       </div>
-      <Image src={image} alt={altText} width={1000} height={1000} className="absolute right-0 bottom-0 max-w-1/2 w-auto h-[650px] object-cover z-10" />
-      {bgImage && (
-        <Image src={bgImage} alt={altText} width={1000} height={1000} className="absolute right-0 bottom-0 max-w-1/2 w-auto h-[650px] object-cover" />
-      )}
+      <div className="h-full w-1/2 relative">
+        <Image src={image} alt={altText} width={1000} height={1000} className="absolute right-0 bottom-0 z-10 max-h-full w-auto" />
+        {bgImage && (
+          <Image src={bgImage} alt={altText} width={1000} height={1000} className="absolute right-0 top-2" />
+        )}
+      </div>
     </div>
   )
 }
