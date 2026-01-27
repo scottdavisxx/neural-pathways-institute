8812c4c9c0e52a91d0f516cd2986ab4ab61cd489
Scott Davis <scottdavisxx@gmail.com>
2026-01-26 18:57:22 -0700

    Update HeroBanner in Patients page with new content and images; add new hero banner assets for enhanced visual appeal.

--- frontend/app/patients/page.tsx
+++ frontend/app/patients/page.tsx
@@ -1,9 +1,25 @@
 import HeroBanner from "../components/HeroBanner";
 
+const heroBanner = {
+  title: "The Breakthrough You've Been Looking For",
+  subtitle: "A mastery-level understanding of brain-to-body function so you can grow your practice and solve cases other practitioners can't",
+  cta: [
+    {
+      href: "/",
+      buttonText: "Become a Practitioner",
+      newTab: false,
+      buttonColor: "brand-yellow"
+    },
+  ],
+  image: "/fpo-hero-banner-2.png",
+  bgImage: "/fpo-hero-banner-bg-2.png",
+  altText: "Hero Banner Image",
+}
+
 export default function Patients() {
   return (
     <>
-      <HeroBanner title="Patients" subtitle="Patients" cta={[]} image="/fpo-hero-banner.png" bgImage="/fpo-hero-banner-bg.png" altText="Hero Banner Image" />
+      <HeroBanner title={heroBanner.title} subtitle={heroBanner.subtitle} cta={heroBanner.cta} image={heroBanner.image} bgImage={heroBanner.bgImage} altText={heroBanner.altText} />
     </>
   )
 }
