946b8ed3177c458798a62b3d52ed3391253a5fd7
Scott Davis <scottdavisxx@gmail.com>
2026-01-27 19:57:04 -0700

    Add CtaBanner component to enhance the Practitioners page with a call-to-action section; include new accent image asset for visual appeal.

---
+++ frontend/app/components/CtaBanner.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Badge from "./ui/Badge";
+import Cta from "./ui/Cta";
+
+export default function CtaBanner() {
+  return (
+    <div className="flex flex-col gap-8 items-center justify-center px-28 py-16 bg-brand-beige text-brand-blue relative">
+      <Image src="/fpo-cta-banner-accent.png" alt="Accent" width={250} height={1000} className="absolute left-0 z-10" />
+      <div className="flex flex-col gap-8 items-center text-center bg-brand-dark-beige rounded-2xl py-16 px-28">
+        <Badge text="For Practitioners" textColor="dark-blue" bgColor="transparent" />
+        <h2 className="text-6xl font-semibold">
+          Is NPI Right for Your Practice?
+        </h2>
+        <p className="text-xl font-satoshi text-black">
+          NPI isn&apos;t only transforming the lives of patients;
+          reshaping the future of practitioners. Instead of memorizing
+          protocols that crumble in real-world practice, clinicians
+          learn a mastery-level skill that elevates their confidence,
+          strengthens their reputation, and grows their practice through
+          undeniable patient success.
+        </p>
+        <p className="text-xl font-satoshi text-black">
+          NPI isn&apos;t only transforming the lives of patients;
+          reshaping the future of practitioners. Instead of memorizing
+          protocols that crumble in real-world practice, clinicians
+          learn a mastery-level skill that elevates their confidence,
+          strengthens their reputation, and grows their practice through
+          undeniable patient success.
+        </p>
+        <Cta href="/" buttonText="Get NPI Certified" newTab={false} buttonColor="brand-teal" />
+      </div>
+    </div>
+  )
+}
--- frontend/app/practitioners/page.tsx
+++ frontend/app/practitioners/page.tsx
@@ -1,9 +1,27 @@
+import CtaBanner from "../components/CtaBanner";
 import HeroBanner from "../components/HeroBanner";
+
+const heroBanner = {
+  title: "The Breakthrough You've Been Looking For",
+  subtitle: "A mastery-level understanding of brain-to-body function so you can grow your practice and solve cases other practitioners can't",
+  cta: [
+    {
+      href: "/",
+      buttonText: "Become a Practitioner",
+      newTab: false,
+      buttonColor: "brand-yellow"
+    },
+  ],
+  image: "/hero-banner-patients-fpo.png",
+  bgImage: "/fpo-hero-banner-bg-2.png",
+  altText: "Hero Banner Image",
+}
 
 export default function Practitioners() {
   return (
     <>
-      <HeroBanner title="Practitioners" subtitle="Practitioners" cta={[]} image="/fpo-hero-banner.png" bgImage="/fpo-hero-banner-bg.png" altText="Hero Banner Image" />
+      <HeroBanner {...heroBanner} />
+      <CtaBanner />
     </>
   )
 }
