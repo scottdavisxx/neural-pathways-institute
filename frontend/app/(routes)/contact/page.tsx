e5dad70a2c6ac6fca475279289e2558ea1e54001
Scott Davis <scottdavisxx@gmail.com>
2026-01-29 21:34:31 -0700

    rethink form, got borders done

--- frontend/app/(routes)/contact/page.tsx
+++ frontend/app/(routes)/contact/page.tsx
@@ -4,9 +4,9 @@ import Cta from "@/app/components/ui/Cta";
 export default function Contact() {
   return (
     <>
-      <div className="flex flex-col gap-4 items-center justify-center h-[599px] mt-20">
+      <div className="flex flex-col gap-4 items-center justify-center mt-20 bg-brand-beige pt-10">
         <h1 className="text-6xl font-semibold leading-snug text-brand-blue">Take the Next Step</h1>
-        <div className="flex gap-4">
+        <div className="flex gap-6">
           <Cta href="/" buttonText="Practitioner" newTab={false} buttonColor="brand-teal" />
           <Cta href="/" buttonText="Patient" newTab={false} buttonColor="brand-teal" />
         </div>
